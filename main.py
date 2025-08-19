c5c13d3ef70a1143aa9903b3cbd7e74eb5028867
studio98hasd <studio98hasd@gmail.com>
2025-08-19 19:41:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask
+import sys
+from libretranslate import main
 
-app = Flask(__name__)
-@app.route("/")
-
-def home():
-    return "LibreTranslate placeholder"
+if __name__ == "__main__":
+    sys.exit(main())
